f10304723712823475dd304a518498c137785a08
Emillyn Muniz <emillyn_muniz@hotmail.com>
2017-09-08 13:30:45 -0300

    Primeiro commit

---
+++ 1001-Extremamente Básico.py
@@ -0,0 +1,6 @@
+#Leia 2 valores inteiros e armazene-os nas variáveis A e B. Efetue a soma de A e B atribuindo o seu resultado na variável X.
+
+A = int(input(''))
+B = int(input(''))
+S = A+B
+print('X = {}'.format(S))
---
+++ 1002 - Área do Círculo.py
@@ -0,0 +1,4 @@
+#A fórmula para calcular a área de uma circunferência é: area = π . raio2. Considerando para este problema que π = 3.14159:
+raio = float(input(''))
+area = 3.14159*(raio**2)
+print('A={:.4f}'.format(area))

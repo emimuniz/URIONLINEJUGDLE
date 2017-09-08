c06a506f986236885ab25bd8165d18b4dfe8e232
Emillyn Muniz <31746775+emimuniz@users.noreply.github.com>
2017-09-08 16:13:02 -0300

    Update 1005 - Media 1.py

--- 1005 - Media 1.py
+++ 1005 - Media 1.py
@@ -1,3 +1,5 @@
+#Leia 2 valores de ponto flutuante de dupla precisão A e B, que correspondem a 2 notas de um aluno.
+#A seguir, calcule a média do aluno, sabendo que a nota A tem peso 3.5 e a nota B tem peso 7.5 (A soma dos pesos portanto é 11).
 A = float(input(''))
 B = float(input(''))
 MEDIA = ((A*3.5)+(B*7.5))/11

0162212dacdbb5b0534294ad4239541389178b94
Emillyn Muniz <emillyn_muniz@hotmail.com>
2017-09-10 13:54:35 -0300

    Primeiros commit

---
+++ 1016 - Distancia.py
@@ -0,0 +1,8 @@
+#Dois carros (X e Y) partem em uma mesma direção. O carro X sai com velocidade constante de 60 Km/h e o carro Y sai com velocidade constante de 90 Km/h.
+#calcule quanto tempo leva (em minutos) para o carro Y tomar essa distância do outro carro.
+
+km = int(input(''))
+x = 60
+y = 90
+minutos = (km/30)*60
+print('%d minutos' % minutos)

23c6f43fde027f460ff3a4f1b3c4f9ec42fc9ebf
Emillyn Muniz <emillyn_muniz@hotmail.com>
2018-02-11 12:48:56 -0200

    primeiros commit

---
+++ 1072 - Intervalo2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intervalo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, contador, numin, nout, nin;
+            Console.Write("");
+            n = int.Parse(Console.ReadLine());
+            contador = 1;
+            nout = 0;
+            nin = 0;
+            while (contador <= n)
+            {
+                contador++;
+                Console.Write("");
+                numin = int.Parse(Console.ReadLine());
+                if (numin >= 10 && numin <= 20)
+                {
+                    nin++;
+                }
+                else
+                {
+                    nout++;
+                }
+
+            }
+
+            Console.WriteLine(nin + " in");
+            Console.WriteLine(nout + " out");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 1073 - Quadrado de Pares.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quadrado_de_Pares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n, cont, par;
+            Console.Write("Digite um numero: ");
+            n = double.Parse(Console.ReadLine());
+            cont = 0;
+            while(cont <= n)
+            {
+                cont++;
+                if (cont % 2 == 0)
+                {
+                    par = Math.Pow(cont, 2);
+                    Console.WriteLine(cont+"^2 = "+par);
+                    cont++;
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}

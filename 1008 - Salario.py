e75ef789d09568561a18f87f9cba91275df563c0
Emillyn Muniz <emillyn_muniz@hotmail.com>
2018-07-04 13:08:06 -0300

    Update 1008 - Salario.py

--- 1008 - Salario.py
+++ 1008 - Salario.py
@@ -1,5 +1,6 @@
-#Escreva um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por hora e calcula o salário desse funcionário. #
-# A seguir, mostre o número e o salário do funcionário, com duas casas decimais.
+#Escreva um programa que leia o número de um funcionário, seu número de horas trabalhadas, 
+#o valor que recebe por hora e calcula o salário desse funcionário. 
+#A seguir, mostre o número e o salário do funcionário, com duas casas decimais.
 
 numf = int(input(''))
 horas = int(input(''))

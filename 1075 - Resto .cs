961455a5c939762db6827d2f6350031930953dfa
Emillyn Muniz <emillyn_muniz@hotmail.com>
2018-07-31 17:35:07 -0300

    Primeiros commit

---
+++ 1075 - Resto .cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, cont;
+            Console.Write("");
+            n = int.Parse(Console.ReadLine());
+            cont = 1;
+            while (cont < 10000)
+            {
+                cont++;
+                if (cont % n == 2)
+                {
+                    Console.WriteLine(cont);
+                    cont++;
+                }
+
+            }
+        }
+    }
+}

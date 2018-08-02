aca8f05bc9a0f771e3ee1dd1a24eb39fef817d63
Emillyn Muniz <emillyn_muniz@hotmail.com>
2018-08-02 14:34:30 -0300

    Primeiros commit

---
+++ 1078 - Tabuada.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, cont;
+            cont = 0;
+            Console.Write("");
+            n = int.Parse(Console.ReadLine());
+            if (n > 2 && 1000 > n)
+            {
+                while (cont < 10)
+                {
+                    cont++;
+                    Console.WriteLine(cont + " x " + n + " = " + cont * n);
+                }
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}

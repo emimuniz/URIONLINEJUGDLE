e024819a8fbc6206eaf3c816b753bc4b7857d40f
Emillyn Muniz <emillyn_muniz@hotmail.com>
2018-08-03 13:44:49 -0300

    Primeiros commit

--- 1078 - Tabuada.cs
+++ 1078 - Tabuada.cs
@@ -10,21 +10,44 @@ namespace ConsoleApp22
     {
         static void Main(string[] args)
         {
-            int n, cont;
-            cont = 0;
-            Console.Write("");
-            n = int.Parse(Console.ReadLine());
-            if (n > 2 && 1000 > n)
+            int localizarLinha = 0, localizarColuna = 1, contagem = 1;
+            float soma = 0, media = 0;
+            char o = char.Parse(Console.ReadLine().ToUpper());
+            float[,] m = new float[12, 12];
+            for (int i = 0; i < 12; i++)
             {
-                while (cont < 10)
+                for (int j = 0; j < 12; j++)
                 {
-                    cont++;
-                    Console.WriteLine(cont + " x " + n + " = " + cont * n);
+                    m[i, j] = float.Parse(Console.ReadLine());
+                    if (j == localizarColuna && i == localizarLinha)
+                    {
+                        if (o == 'S')
+                        {
+                            soma += m[i, j];
+                        }
+                        else if (o == 'M')
+                        {
+                            soma += m[i, j];
+                            media = soma / 66;
+                        }
+                        localizarColuna++;
+                    }
                 }
+                contagem++;
+                localizarColuna = contagem;
+                localizarLinha++;
+            }
+            if (o == 'S')
+            {
+                Console.WriteLine("{0:f1}", soma);
+            }
+            else if (o == 'M')
+            {
+                Console.WriteLine("{0:f1}", media);
             }
 
             Console.ReadKey();
-
         }
+
     }
 }
---
+++ 1079 - Medias Ponderadas.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, cont;
+            float p1, p2, p3;
+            n = int.Parse(Console.ReadLine());
+            cont = 0;
+            while (cont < n)
+            {
+                cont++;
+                string[] linha = Console.ReadLine().Split(' ');
+                p1 = float.Parse(linha[0]);
+                p2 = float.Parse(linha[1]);
+                p3 = float.Parse(linha[2]);
+                float media = (p1 * 2 + p2 * 3 + p3 * 5) / 10;
+                Console.WriteLine("{0:f1}", media);
+
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}

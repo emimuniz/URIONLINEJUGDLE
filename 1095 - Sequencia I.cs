e6a2d9fdcd337eb6b4677d405a4f19dbc70e19f2
Emillyn Muniz <emillyn_muniz@hotmail.com>
2018-08-06 16:03:55 -0300

    Primeiros commit

---
+++ 1095 - Sequencia I.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, cont;
+            i = 1;
+            j = 60;
+            cont = 1;
+            while (cont <= 13)
+            {
+                cont++;
+                Console.WriteLine("I=" + i + " J=" + j);
+                i = i + 3;
+                j = j - 5;
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

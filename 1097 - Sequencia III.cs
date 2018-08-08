7e9b1a77f6e35ad3f5a2df12f6d172c4504d1b69
Emillyn Muniz <emillyn_muniz@hotmail.com>
2018-08-08 16:16:31 -0300

    Primeiros commit

---
+++ 1097 - Sequencia III.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float I, J, cont1;
+            I = 1;
+            J = 7;
+            cont1 = 0;
+            while (I <= 9)
+            {
+
+                while (cont1 < 3)
+                {
+
+                    Console.WriteLine("I=" + I + " J=" + J);
+                    J--;
+                    cont1++;
+                }
+                cont1 = 0;
+                I += 2;
+                J += 5;
+            }
+
+
+            Console.Read();
+        }
+    }
+}

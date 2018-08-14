9a9252bde28bcd1eeadfcd49164ed9b113d58662
Emillyn Muniz <emillyn_muniz@hotmail.com>
2018-08-14 16:52:38 -0300

    Primeiros commit

---
+++ 1113 - Crescente e Decrescente.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp36
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X, Y;
+            X = 0;
+            Y = 1;
+            while (X != Y)
+            {
+                string[] linha = Console.ReadLine().Split(' ');
+                X = int.Parse(linha[0]);
+                Y = int.Parse(linha[1]);
+
+                if (X > Y)
+                {
+                    Console.WriteLine("Decrescente");
+
+                }
+                if (Y > X)
+                {
+                    Console.WriteLine("Crescente");
+
+                }
+
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

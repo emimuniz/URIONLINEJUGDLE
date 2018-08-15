ed611fb931f5b24fcb74891ef2fcc5c9b877f266
Emillyn Muniz <emillyn_muniz@hotmail.com>
2018-08-15 13:56:18 -0300

    Primeiros commit

---
+++ 1114 - Senha Fixa.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int senha;
+            while (true)
+            {
+                Console.Write("");
+                senha = int.Parse(Console.ReadLine());
+                if (senha == 2002)
+                {
+                    Console.WriteLine("Acesso Permitido");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Senha Invalida");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

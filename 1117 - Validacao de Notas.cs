bae9ea38e041d5db010ad87ae4bd06cee3104c04
Emillyn Muniz <emillyn_muniz@hotmail.com>
2018-08-20 19:47:31 -0300

    Primeiros commit

---
+++ 1117 - Validacao de Notas.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp47
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float nota1, nota2;
+            nota1 = float.Parse(Console.ReadLine());
+            while (nota1 < 0 || nota1 > 10)
+            {
+                Console.WriteLine("nota invalida");
+                nota1 = float.Parse(Console.ReadLine());
+            }
+
+            nota2 = float.Parse(Console.ReadLine());
+
+            while (nota2 < 0 || nota2 > 10)
+            {
+                Console.WriteLine("nota invalida");
+                nota2 = float.Parse(Console.ReadLine());
+            }
+
+            float media = (nota1 + nota2) / 2;
+            Console.WriteLine("media = {0:f2}", media);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
